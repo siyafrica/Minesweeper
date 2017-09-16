866b2607e6e3006ecbd4ef4496eaa8c9704484e6
siyafrica <siyafrica@gmail.com>
2017-09-16 15:01:52 +0200

    first part of the minesweeper project completed

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+//create a blank line using pipes and spaces
+const blankLine = '  |   |  ';
+
+//a guess line for when a player guesses by clicking the first square of this row
+const guessLine = '1 |   |  ';
+
+//a bomb line represents what a board will look like when a player clicks and reveals a bomb
+const bombLine = '  | B |  ';
+
+console.log(`This is what a board with a guess and a bomb on it would look like: ` );
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
